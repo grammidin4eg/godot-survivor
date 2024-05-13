d73145523e779db2db786181639030c3873bdfd5
Karachevtsev Yu Yu <grammidin4eg@gmail.com>
2024-05-13 14:56:51 +0300

    анимация гусениц

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,6 +4,15 @@ extends CharacterBody2D
 const SPEED = 300.0
 var platform_rotation = 0
 
+func runAnimation():
+	$Track1.play("run")
+	$Track2.play("run")
+
+func stopAnimation():
+	$Track1.stop()
+	$Track2.stop()
+
+
 func _physics_process(delta):
 	var direction_horizontal = Input.get_axis("ui_left", "ui_right")
 	var direction_vertical = Input.get_axis("ui_up", "ui_down")
@@ -11,11 +20,14 @@ func _physics_process(delta):
 	if direction_horizontal != 0:
 		velocity = Vector2.RIGHT * direction_horizontal * SPEED
 		rotation_degrees = 90 if direction_horizontal > 0 else -90
+		runAnimation()
 	elif direction_vertical != 0:
 		velocity = Vector2.DOWN * direction_vertical * SPEED
 		rotation_degrees = 180 if direction_vertical > 0 else 0
+		runAnimation()
 	else:
 		velocity = Vector2.ZERO
+		stopAnimation()
 	
 	%top.look_at(get_global_mouse_position())
 	%top.rotate(90)
