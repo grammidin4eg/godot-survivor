6b3733706537a37abf227afe21379c0ce11847f5
Karachevtsev Yu Yu <grammidin4eg@gmail.com>
2024-05-14 14:08:31 +0300

    задержка стрельбы

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const BULLET = preload("res://scenes/bullet.tscn")
 
-var angle_fix = 1.5
+var bullet_timer : Timer = Timer.new()
 
 func runAnimation():
 	$Track1.play("run")
@@ -14,6 +14,10 @@ func stopAnimation():
 	$Track1.stop()
 	$Track2.stop()
 
+func _ready():
+	bullet_timer.wait_time = 0.5
+	bullet_timer.one_shot = true
+	add_child(bullet_timer)
 
 func _physics_process(delta):
 	var direction_horizontal = Input.get_axis("ui_left", "ui_right")
@@ -35,7 +39,8 @@ func _physics_process(delta):
 	%top.rotate(PI / 2)
 	move_and_slide()
 	
-	if Input.is_action_pressed("fire"):
+	if Input.is_action_pressed("fire") and bullet_timer.is_stopped():
+		bullet_timer.start()
 		var bullet = BULLET.instantiate()
 		bullet.global_position = %top/BulletSpawnPoint.global_position
 		bullet.global_rotation = %top.global_rotation
