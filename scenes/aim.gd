5abf5d695e479099f82068a777ca1195f391e693
Karachevtsev Yu Yu <grammidin4eg@gmail.com>
2024-05-15 22:35:00 +0300

    тарелки

--- scenes/aim.gd
+++ scenes/aim.gd
@@ -1,11 +1,41 @@
-extends Area2D
+extends CharacterBody2D
 
 const BOOM = preload("res://scenes/boom.tscn")
+const SPEED = 30
+
+func _ready():
+	$Sprite.play("default")
+	
+func _physics_process(delta):
+	if %Player:
+		var direction = global_position.direction_to(%Player.global_position)
+		velocity = direction * SPEED
+		move_and_slide()
+	
+	# Посмотрим, какие есть коллизии
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		#var enemy : Enemy = collider as Enemy
+
+		if collider.is_in_group("Bullet"):
+			_on_bullet_entered(collider)
+		
+		if collider.is_in_group("Player"):
+			_on_player_entered(collider)
+
+func _create_boom(position):
+	var boom =BOOM.instantiate()
+	boom.global_position = position
+	get_parent().add_child(boom)
+	boom.play('default')
 
 func _on_bullet_entered(bullet):
 	bullet.queue_free()
-	var boom =BOOM.instantiate()
-	boom.global_position = global_position
-	get_parent().add_child(boom)
-	boom.play('default')
+	_create_boom(global_position)
 	queue_free()
+	
+func _on_player_entered(player):
+	queue_free()
+	_create_boom(player.global_position)
+	player.queue_free()	
